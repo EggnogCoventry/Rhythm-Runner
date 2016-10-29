a5c0bd2d492128771ffe888d373e18b720b45823
AdamSmedley <adam.smedley@hotmail.com>
2016-10-29 14:55:20 +0100

    Working spawner and ground scripts using a prefab.

---
+++ Spawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+
+    public Object Sands;
+    public float wavecountdown = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wavecountdown <= 0)
+        {
+            Vector3 position = new Vector3(20, -4, 0);
+            Instantiate(Sands, position, Quaternion.identity);
+            wavecountdown = 3;
+        }
+
+        else
+        {
+            wavecountdown -= Time.deltaTime;
+        }
+
+
+    }
+}
