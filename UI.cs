1b6e8fedd8f80991c5f6ff0ffd9a658dc30107c7
AdamSmedley <adam.smedley@hotmail.com>
2016-11-15 11:06:04 +0000

    Add files via upload

---
+++ UI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+    Text Txt;
+    private float Score = 0;
+
+	// Use this for initialization
+	void Start () {
+        Txt = gameObject.GetComponent<Text>();
+        Txt.text = "Score: " + Score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Score += Time.deltaTime;
+        Txt.text = "Score : " + Mathf.Round(Score);
+    }
+}
